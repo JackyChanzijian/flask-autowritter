11f2f5c872ab6a5680d5bac6253e21ba36ee16c0
Jacky Chan <1303213163@qq.com>
2022-03-12 23:50:32 +0800

    Style and make it work using only one button

--- __init__.py
+++ __init__.py
@@ -4,31 +4,33 @@ from flask import Flask, render_template, request
 from transformers import (
     AutoTokenizer,
     pipeline,
-    AutoModelForSeq2SeqLM
-
+    AutoModelForCausalLM
 )
 app = Flask(__name__)
 
 tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
-model = AutoModelForSeq2SeqLM.from_pretrained("distilgpt2")
-writer = pipeline("text-generation", model=model, tokenizer=tokenizer, num_return_sequences=3);
+model = AutoModelForCausalLM.from_pretrained("distilgpt2")
+writer = pipeline("text-generation", model=model, tokenizer=tokenizer);
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    choice:int = 1;
+    choice:int = 0
     if request.method == 'POST':
-        if request.form.get('1') is not None:
-            choice = 1
-        if request.form.get('2') is not None:
-            choice = 2
-        if request.form.get('3') is not None:
-            choice = 3
-        input = request.form['name']
+        # if request.form.get('1') is not None:
+        #     choice = 1
+        # if request.form.get('2') is not None:
+        #     choice = 2
+        # if request.form.get('3') is not None:
+        #     choice = 3
+        input = request.form['input']
         return render_template("index.html", output_text=getArticle(input, choice))
     return render_template("index.html", output_text="")
 
 def getArticle(article:string, index:int) -> string:
-    output_text = writer(article, do_sample=False);
-    return output_text[index]["generated_text"]
+    encoded_input = tokenizer(article)
+    word_count:int = len(encoded_input['input_ids'])
+    output_text = writer(article,  min_length=word_count + 10, max_length=word_count + 30)
+    print(word_count)
+    return output_text[0]["generated_text"]
     
 if (__name__ == "__main__"):
-    app.run(host="0.0.0.0", port=81)
+    app.run(port=8081)
