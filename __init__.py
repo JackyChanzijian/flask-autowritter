3ca0d6360db060f745b88e5ba653d161a42abb4b
JackyChanzijian <52320621+JackyChanzijian@users.noreply.github.com>
2022-03-12 12:57:59 +0800

    Basic change for autowritter

--- __init__.py
+++ __init__.py
@@ -9,29 +9,26 @@ from transformers import (
 )
 app = Flask(__name__)
 
-tokenizer = AutoTokenizer.from_pretrained("cnicu/t5-small-booksum")
-model = AutoModelForSeq2SeqLM.from_pretrained("cnicu/t5-small-booksum")
-summarizer = pipeline("summarization", model=model, tokenizer=tokenizer);
-article = """
-Happy womens day to all the females in this comment section!
-
-Also once again loved your video mysticat, keep growing your channel with your amazing content, I have a suggestion, can you make these cursed things into a datapack, like just run one command and see the magic happening and armor stands spawning!, you could even customise it for all shapes, like you accidentally did for the hexagon!
-I would really love to try the circle and this triangle, and even the shark, but Im too dumb too successfully copy you lol
-"""
+tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
+model = AutoModelForSeq2SeqLM.from_pretrained("distilgpt2")
+writer = pipeline("text-generation", model=model, tokenizer=tokenizer, num_return_sequences=3);
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    choice:int = 1;
     if request.method == 'POST':
+        if request.form.get('1') is not None:
+            choice = 1
+        if request.form.get('2') is not None:
+            choice = 2
+        if request.form.get('3') is not None:
+            choice = 3
         input = request.form['name']
-        return render_template("index.html", summary=getSummary(input))
-    return render_template("index.html", summary="")
+        return render_template("index.html", output_text=getArticle(input, choice))
+    return render_template("index.html", output_text="")
 
-@app.route("/<article>")
-def summary_page(article):
-    return getSummary(article)
-
-def getSummary(article:string) -> string:
-    summary = summarizer(article, do_sample=False);
-    return summary[0]["summary_text"]
+def getArticle(article:string, index:int) -> string:
+    output_text = writer(article, do_sample=False);
+    return output_text[index]["generated_text"]
     
 if (__name__ == "__main__"):
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=81)
